68448b71ac46a49a48563c44738143ff6fef7bd9
Richard <richardwangwang@gmail.com>
2014-12-13 19:19:28 +0800

    Added table init script.

---
+++ django_sheet/static/js/django_sheet.js
@@ -0,0 +1,35 @@
+var serverJson = {"max_row": 20, "max_col": 5, data:[{"row":1, "col":1, "value": "good"}]}
+
+
+function getEmptyElemInArray(n){
+    var res = [];
+    for(var index=0; index<n; ++index){
+        res[index] = "";
+    }
+    return res;
+}
+
+
+function loadJson(json){
+    var tableData = [];
+    var maxRow = json["max_row"];
+    var maxCol = json["max_col"];
+    for(var row=0; row<maxRow; ++row){
+        tableData[row] = getEmptyElemInArray(maxCol);
+    }
+    var dataArray = json["data"];
+    for(var index=0; index<dataArray.length; ++index){
+        var data = dataArray[index];
+        tableData[data["row"]][data["col"]] = data["value"];
+    }
+    var tableStr = "";
+    for(var rowIndex=0; rowIndex<maxRow; ++rowIndex){
+        tableStr +="<tr>";
+        for(var colIndex=0; colIndex<maxCol; ++colIndex){
+            tableStr += "<td>"+tableData[rowIndex][colIndex]+"</td>";
+        }
+        tableStr +="</tr>";
+    }
+    return tableStr;
+}
+
--- django_sheet/urls.py
+++ django_sheet/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from djangoautoconf.auto_conf_urls import add_default_root_url
+from djangoautoconf.tastypie_utils import create_tastypie_resource
+from django_sheet.models import Sheet, Cell, DateCellValue, FloatCellValue, StringCellValue, IntegerCellValue, SheetFile
 from views import SpreadSheetTemplateView
 
 __author__ = 'weijia'
@@ -7,6 +9,13 @@ __author__ = 'weijia'
 
 urlpatterns = patterns('',
     url(r'^$', SpreadSheetTemplateView.as_view(), name='demo_home'),
+    url(r'^api/', include(create_tastypie_resource(SheetFile).urls)),
+    url(r'^api/', include(create_tastypie_resource(Sheet).urls)),
+    url(r'^api/', include(create_tastypie_resource(Cell).urls)),
+    url(r'^api/', include(create_tastypie_resource(DateCellValue).urls)),
+    url(r'^api/', include(create_tastypie_resource(FloatCellValue).urls)),
+    url(r'^api/', include(create_tastypie_resource(StringCellValue).urls)),
+    url(r'^api/', include(create_tastypie_resource(IntegerCellValue).urls)),
 )
 
 add_default_root_url("^django_sheet/")
